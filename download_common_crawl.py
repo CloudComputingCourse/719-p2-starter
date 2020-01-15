62f517d69ca1d3aece207b03d2d788330b1f6f2c
Yu Wang <yuw1995@gmail.com>
2020-01-15 04:00:00 +0000

    Change the download_crawl

--- download_common_crawl.py
+++ download_common_crawl.py
@@ -2,17 +2,18 @@
 
 from datetime import datetime
 import threading
-from urllib import urlretrieve
+from urllib.request import urlretrieve
 import os
 import sys
 import string
 
 base_url = "https://commoncrawl.s3.amazonaws.com/"
-hdfs_exe = "/root/hadoop/bin/hdfs dfs"
+hdfs_exe = "/home/ec2-user/hadoop/bin/hdfs dfs"
 
 # create the following two directories
 hdfs_base_url = "/common_crawl_wet/"
-scratch_dir = "/root/tmp/"
+# scratch_dir = "/root/tmp/"
+scratch_dir = "/data/"
 
 start_time = datetime.now()
 
@@ -22,42 +23,44 @@ def log(format_string, *args):
     formatter = string.Formatter()
     header = "[%ds] [%s] " % (seconds_elapsed, threading.current_thread().name)
     full_format_string = header + format_string
-    print full_format_string % args
+    print(full_format_string % args)
 
 def download_path_list(path_list):
     log("starting to download %d files", len(path_list))
     for path in path_list:
-	file_name = path.replace("/", "_")
-	urlretrieve(base_url + path, scratch_dir + file_name)
-	os.system(hdfs_exe + " -put " + scratch_dir + file_name + " " + hdfs_base_url + file_name)
-	os.system("rm -rf " + scratch_dir + file_name)
-	log("file %s downloaded" % file_name)
+        file_name = path.replace("/", "_")
+        if not os.path.exists(scratch_dir + file_name):
+            urlretrieve(base_url + path, scratch_dir + file_name)
+            log("downloaded: file %s" % file_name)
+        else:
+            log("cached: file %s" % file_name)
+        os.system(hdfs_exe + " -put " + scratch_dir + file_name + " " + hdfs_base_url + file_name)
     log("done")
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print "Usage: %s [path_file] [num_threads (typically 2)]"
+        print("Usage: %s [path_file] [num_threads (typically 2)]" % sys.argv[0])
         sys.exit(0)
 
     path_file = sys.argv[1]
     num_threads = int(sys.argv[2])
     path_list = []
     with open(path_file, "r") as path_fobj:
-	for path in path_fobj:
-	    path_list.append(path.strip())
+        for path in path_fobj:
+            path_list.append(path.strip())
 
     num_threads = min(num_threads, len(path_list))
     num_files_per_thread = (len(path_list) + num_threads - 1) / num_threads
     start = 0
     threads = []
     for i in range(0, num_threads):
-	end = min(len(path_list), start + num_files_per_thread)
-	thread = threading.Thread(target=download_path_list, name="worker-" + str(i),
-				  args=(path_list[start:end],))
-	thread.start()
-	threads.append(thread)
-	start = end
+        end = int(min(len(path_list), start + num_files_per_thread))
+        thread = threading.Thread(target=download_path_list, name="worker-" + str(i),
+                    args=(path_list[start:end],))
+        thread.start()
+        threads.append(thread)
+        start = end
     for thread in threads:
-	thread.join()
+	    thread.join()
 
     log("%d files are downloaded", len(path_list))
--- get_WARC_dataset.sh
+++ get_WARC_dataset.sh
@@ -5,7 +5,7 @@
 set -e
 if [ -z ${1+x} ]; then
   echo "USAGE"
-  echo '$1 - N - The fist N of the WARC files to get'
+  echo "$1 - N - The fist N of the WARC files to get"
   exit
 fi
 
@@ -17,15 +17,25 @@ WET_TOP_N_PATHS=wet.paths.top_$1
 STARTER_CODE_DIR=.
 
 # Run
+if [[ $(sudo file -s /dev/xvdp) == "/dev/xvdp: data" ]]; then
+  echo "Initializing File System"
+  sudo mkfs -t xfs /dev/xvdp
+fi
+if [[ ! $(cat /proc/mounts | grep /dev/xvdp) ]]; then
+  if [ ! -d "/data" ]; then
+    sudo mkdir /data
+  fi
+  echo "Mount /dev/xvdp to /data"
+  sudo mount /dev/xvdp /data
+  sudo chown ec2-user:ec2-user /data
+fi
 echo "Getting WARC paths data"
 wget $WET_PATH_URL
 echo "Unzipping WARC paths data"
-gunzip -k -f $WET_PATH
+gunzip -f $WET_PATH
 echo "Writing top N of WARC paths file: ${WET_TOP_N_PATHS}"
 head -"$1" $WET_PATH_UNZIP > $WET_TOP_N_PATHS
 echo "Creating HDFS dir"
-/root/hadoop/bin/hdfs dfs -mkdir -p /common_crawl_wet/
-echo "Creating /root/tmp dir"
-mkdir -p /root/tmp
+/home/ec2-user/hadoop/bin/hdfs dfs -mkdir -p /common_crawl_wet/
 echo "Downloading Rest of WARC Data"
-python $STARTER_CODE_DIR/download_common_crawl.py $WET_TOP_N_PATHS 2
+python3 $STARTER_CODE_DIR/download_common_crawl.py $WET_TOP_N_PATHS 2
