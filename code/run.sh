dd8e10ddbf6b41d5079593975be147618302969e
Ankush Jain <anku.94@gmail.com>
2020-01-29 22:41:27 +0000

    Update run.sh w historyServer flags

--- code/run.sh
+++ code/run.sh
@@ -3,6 +3,11 @@
 #                   $1          $2            $3                  $4                  $5              $6
 
 # -------- BEGIN: DON'T CHANGE --------
+export HADOOP_HOME=$HOME/hadoop
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HADOOP_HOME/lib/native
+
+echo "Hadoop homedir: $HADOOP_HOME"
+
 SPARK_SUBMIT="spark-submit"
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ETL_SCRIPT="$PROJECT_DIR/spark_etl.py"
@@ -15,6 +20,7 @@ ETL_SCRIPT="$PROJECT_DIR/spark_etl.py"
 master_url=`curl -s http://169.254.169.254/latest/meta-data/public-hostname`
 
 $SPARK_SUBMIT \
+    --conf spark.eventLog.enabled=true \
     --deploy-mode client \
     --master spark://$master_url:7077 \
     $ETL_SCRIPT \
